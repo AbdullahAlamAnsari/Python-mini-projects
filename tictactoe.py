ef2a920b6e0a1d6f037a8c9aef632dbd573be8f2
Abdullah Alam Ansari <abdullahalamofc57@gmail.com>
2026-02-02 22:14:46 +0500

    TIC TAC TOE DONE

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,8 @@
-board = [" ", " ", " ",
-         " ", " ", " ",
-         " ", " ", " "]
+board = [" "," "," ",
+         " "," "," ",
+         " "," "," "]
+
+
 
 def show_board():
     print()
@@ -12,37 +14,34 @@ def show_board():
     print()
 
 def check_win(player):
-    win_conditions = [
-        (0,1,2), (3,4,5), (6,7,8),
-        (0,3,6), (1,4,7), (2,5,8),
-        (0,4,8), (2,4,6)
-    ]
-    for a, b, c in win_conditions:
-        print(a)
-        print(b)
-        print(c)
+    possible_wins = [(0,1,2),(3,4,5),(6,7,8),                #check the winning by putting index to the board and match the signs X or O
+                     (0,3,6),(1,4,7),(2,5,8),
+                     (0,4,8), (2,4,6)]
+
+    for a,b,c in possible_wins:
         if board[a] == board[b] == board[c] == player:
             return True
     return False
-
-player = "X"
-
-for turn in range(9):
+moves = 0
+choose_player = input("With Which Player Do You Want To Start With\nChoose X or O: ")
+player = choose_player
+while moves<9:
     show_board()
     pos = int(input("Player " + player + ", enter position (1-9): ")) - 1
 
     if board[pos] == " ":
         board[pos] = player
+        moves +=1 #increments the move
     else:
         print("Position already taken!")
         continue
 
-    if check_win(player):
+    if check_win(player): #function return true or false in case of a win!
         show_board()
         print("Player", player, "wins!")
         break
 
-    if player == "X":
+    if player == "X": #switches between players!
         player = "O"
     else:
         player = "X"
